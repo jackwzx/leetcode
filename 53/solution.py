dbf8ea38bd47ac88f88a6982d4b61fa4a7aa024e
jackwangliang <jackwangliang@didichuxing.com>
2019-12-09 19:55:03 +0800

    add 53

---
+++ 53/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+class Solution(object):
+    def maxSubArray(self, nums):
+
+        if len(nums) == 0:
+            return 0
+
+        preSum = maxSum = nums[0]
+        for i in xrange(1, len(nums)):
+            preSum = max((preSum + nums[i]), nums[i])
+            print("i=%d, preSum=%d"%(i, preSum))
+            maxSum = max(maxSum, preSum)
+            print("i=%d, maxSum=%d"%(i, maxSum))
+        return maxSum
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    print result
